0f55a8709f34e22d766f740eb07905fa6a0ffa2f
akunze-matc <akunze4@madisoncollege.edu>
2022-02-10 11:30:45 -0600

    Week4's final commit

--- week4/sequences.py
+++ week4/sequences.py
@@ -10,7 +10,7 @@ varString = "Here is a nice string to slice"
 varList = ["Here", "is", "a", "nice", "list", "to", "slice"]
 
 #2
-print(f"{varString[3::]}\n {varString[0:3]}\n {varString[3:12]}\n {varString[::2]}\n {varString[::-1]}")
+print(f"{varString[3::]}\n{varString[0:3]}\n{varString[3:12]}\n{varString[::2]}\n{varString[::-1]}")
 
 #3
 print(f"{varList[::-1]}")
@@ -21,8 +21,8 @@ print(f"{varList[1::]}")
 
 #4
 for i in varString.split(" "):
-    print(f"{i}\n")
+    print(f"{i}")
 
 #5
 for i in varList:
-    print(f"{i}\n")
+    print(f"{i}")
