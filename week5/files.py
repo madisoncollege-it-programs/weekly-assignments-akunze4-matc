8ce8bccdc4d73421ed868ae13dec42e75c07f74a
akunze-matc <akunze4@madisoncollege.edu>
2022-02-17 11:57:46 -0600

    Ready for Grading

--- week5/files.py
+++ week5/files.py
@@ -2,7 +2,7 @@
 #Author: Alex Kunze, #Purpose: Managing files practice
 
 
-# A
+print("Question A below\n")
 
 
 openFile = open("/etc/passwd", "r")
@@ -10,39 +10,38 @@ openFile = open("/etc/passwd", "r")
 unifiedfileString = openFile.read()
 print(f"Number of items in the single unified string - {len(unifiedfileString)}")
 print("We want to use the read() function when we want to put all the contents of a given file into a string ")
+print(f"\n")
 
 openFile.close()
 
 
-# B
 
+print("Question B below\n")
 
-print(f"\n\n")
 
 openFile = open("/etc/passwd", "r")
 
 filenewlineString = openFile.readlines()
 print(f"Number of lines within in /etc/passwd list - {len(filenewlineString)}")
 print("We want to use the readlines() function, when we want to put the contents of a file into a list.")
+print(f"\n")
 
 openFile.close()
 
 
-# C
+print("Question C below")
 
 
 openFile = open("/etc/passwd", "r")
-
- fileonelineStringer = openFile.readline()
-
 count = 0
-for line in fileonelineStringer:
-    count += 1
-print(count)
+for line in openFile:
+    #print(f"{openFile.readline()}", end='')
+    count += len(line)
+
+print("\n")
+print(f"Number of items within the /etc/passwd file - {count}")
+print("We use this method when we need to loop over an extremely large file,")
+print("without the need to open the entire file at once!")
+
 
 openFile.close()
-
-
-
-
-
