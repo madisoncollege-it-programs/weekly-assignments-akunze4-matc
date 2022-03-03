7bf6ccda7d686e38922809d9d05197d9bf5413b0
akunze-matc <akunze4@madisoncollege.edu>
2022-03-03 10:27:14 -0600

    Ready for Grading 3-3-2022

--- week7/dictonaries.py
+++ week7/dictonaries.py
@@ -3,6 +3,7 @@
 #Purpose: Messing with Dictonaries
 
 #1
+print(f"Step1: -----This is just setting up the Dictonary-----")
 myDict = {
         "server1.testlab.com": "192.168.1.10",
         "server2.testlab.com": "192.168.1.11",
@@ -13,21 +14,27 @@ myDict = {
 }
 
 #2
-# print(myDict.keys())
+print(f"Step2: -----------------------------------------------")
+print(myDict.keys())
 
 #3
-# for item in sorted(myDict):
-#     print(myDict[item])
+print(f"Step3: -----------------------------------------------")
+for item in sorted(myDict):
+    print(myDict[item])
 
-#4
-# print(myDict.items())
+#4  
+print(f"Step4: -----------------------------------------------")
+print(myDict.items())
 
 #5
+print(f"Step5: Adding new values to the dictonary-------------")
 myDict["server7.testlab.com"] = "192.168.1.16"
 myDict["server8.testlab.com"] = "192.168.1.17"
 
 #6
-# print("server7.testlab.com" in myDict.keys())
+print(f"Step6: -----------------------------------------------")
+print("server7.testlab.com" in myDict.keys())
 
 #7
+print(f"Step7: -----------------------------------------------")
 print("server10.testlab.com" in myDict.keys())
