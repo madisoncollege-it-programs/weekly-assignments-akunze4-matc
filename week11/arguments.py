300f3f74c0995301d8233ad338abafa23f1bdb0f
sarcasticdream <kayakalotmlg@gmail.com>
2022-04-06 17:12:52 -0500

    Not Ready for Grading

---
+++ week11/arguments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#Author: Alex Kunze, Purpose: Learning ArgParse
+
+import argparse
+
+parser = argparse.ArgumentParser(description="This is Alex Kunzes' script")
+
+parser.add_argument('-m', dest='Basic',type=str, help="Enter some text")
+parser.add_argument('-i', '--integer', dest='Integer',type=int, required=True, help="<required> Enter a simple Integer")
+parser.add_argument('-d', '--float', dest='Float', type=float, help="Enter a float")
+parser.add_argument('-s', '--string', dest='String', type=str, help="Enter a string")
+parser.add_argument('-l', dest='Strings', nargs='+', help="Enter a list of strings (space delimited)")
+parser.add_argument('-t','--set-true', default=False, help='Toggle from default False to True')
+parser.add_argument('-f','--set-false', default=True, help='Toggle from default True to False')
+parser.add_argument('-v', '--version', help="shows program's version number and exit")
+
+input_results = parser.parse_args()
+
+print(input_results)
+
+print(f"Basic entry: {input_results.Basic}")
+print(f"Integer Entry: {input_results.Integer}")
+print(f"Float Entry: {input_results.Float}")
+print(f"String Entry: {input_results.String}")
+print(f"Strings Entry: {input_results.Strings}")
