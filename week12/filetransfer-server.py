3aaa977246697132ccd7d0f6e8393155635fe5ed
sarcasticdream <kayakalotmlg@gmail.com>
2022-04-12 22:37:56 -0500

    Not quite ready for grading yetti

---
+++ week12/filetransfer-server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+'''
+Name: Alex Kunze
+Purpose: Acting as a client side for the file transfer
+NOTE: I used to base for the client from the LAB
+'''
+
+import socket
+
+LHOST = ''
+LPORT = 5000
+RCV_DATA = ""         	
+
+L_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+L_SOCK.bind((LHOST, LPORT))
+
+while True:
+    	L_SOCK.listen(1)
+    	L_CONN, addr = L_SOCK.accept()
+    	print('Connected by', addr)
+    	while True:
+            RCV_DATA = L_CONN.recv(1024)
+            if not RCV_DATA: break
+            x = input("Enter filename to which the file shall be copied to: ")
+            print("Thanks for your cooperation")
+            weirdVar = open(x, "wb")
+            weirdVar.write(RCV_DATA)
+            weirdVar.close()
+            # This line sends the data back to the client
+            L_CONN.sendall(RCV_DATA)
+
+    	L_CONN.close()
+
