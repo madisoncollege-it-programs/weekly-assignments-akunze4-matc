f896152de82e5268aac225441f0b49022892ec6f
sarcasticdream <kayakalotmlg@gmail.com>
2022-04-20 23:04:05 -0500

    Possibly read for grading

--- week13/labAssignments/inspect-web.py
+++ week13/labAssignments/inspect-web.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+import requests,bs4
+
+res = requests.get('https://notpurple.com')
+res.raise_for_status()
+myHTML = bs4.BeautifulSoup(res.text,features="html.parser")
+# print(type(myHTML))
+print(myHTML.title.text)
+print(myHTML.find_all('a'))
