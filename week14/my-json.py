4a3175ff5ba88966441800a90c6ddd10c61b6123
sarcasticdream <kayakalotmlg@gmail.com>
2022-04-26 11:50:41 -0500

    Ready for Grading

---
+++ week14/my-json.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#Name: Alex Kunze
+#Purpose: Using Json
+
+import argparse
+
+parser = argparse.ArgumentParser(description="This is Alex Kunzes' script")
+
+parser.add_argument('-m', dest='Basic',type=str, help="Enter some text")
+parser.add_argument('-ip', '--ip-address', dest='Ip', type=str,metavar="[a string]", help="Enter a simple string")
+parser.add_argument('-v', '--version',action='version',version='1.0', help="shows program's version number and exit")
+
+input_results = parser.parse_args()
+
+newVar = input_results.Ip
+
+import sys
+import requests
+import json
+
+# Create the url that we want to connect to
+# url = f"http://ipinfo.io/{sys.argv[1]}/json"
+url = f"http://ipinfo.io/{newVar}/json"
+
+# Send the "get" request to the web server
+jsonResp = requests.get(url)
+# Convert the returned json formatted text to a dictionary
+dictResp = json.loads(jsonResp.text) 
+
+# for each key in the dictionary print the key and the data
+for key in dictResp.keys():
+    print(f"{key: <10}: {dictResp[key]: <10}")
+
